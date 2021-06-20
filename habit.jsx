cc04e4b635880355790c4c08f0b8ec3fe81842cf
youngbiny <85481707+youngbiny@users.noreply.github.com>
2021-06-20 23:09:47 +0900

    Add files via upload

--- habit.jsx
+++ habit.jsx
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
+
+    handleIncrement = () => {
+       this.props.onIncrement(this.props.habit);
+    };
+    hadleDecrease = () => {
+        this.props.onDecrement(this.props.habit);
+    };
+    hadleDelete = () => {
+        this.props.onDelete(this.props.habit);
+    };
+
     render() {
+        console.log(this.props.habit);
+        const habitName = this.props.habit.name;
+        const {name, count} = this.props.habit;
         return (
-        <>
-            <span className="habit-name">Reading</span>
-            <span className="habit-count">8</span>
-            <span className="habit-coding">coding</span>
-        </>
+        <li className="habit">
+            <span className="habit-name">{name}</span>
+            <span className="habit-count">{count}</span>
+            <button className="habit-button habit-increase" onClick={this.handleIncrement}>
+                <i className="far fa-plus-square"></i>
+            </button>
+            <button className="habit-button habit-decrease" onClick={this.hadleDecrease}>
+                <i className="far fa-minus-square"></i>
+            </button>
+            <button className="habit-button habit-delete" onClick={this.hadleDelete}>
+                <i className="fas fa-trash"></i>
+            </button>
+        </li>
         );
     }
 }
