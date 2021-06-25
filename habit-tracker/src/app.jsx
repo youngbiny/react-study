0e5117e53036b57a104262f04f3d514521de4666
youngbiny <ultrabab@naver.com>
2021-06-25 23:08:45 +0900

    app

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,8 +1,49 @@
+import React, {Component} from 'react';
 import './app.css';
 import Habits from './compoents/habits';
+import Navbar from './compoents/navbar';
 
-function App() {
-    return <Habits />;
+class App extends Component {
+  state = {
+      habits: [
+          { id: 1, name : 'Reading' , count :0},
+          { id: 2, name : 'Running' , count :0},
+          { id: 3, name : 'Coding' , count :0},
+      ],
+  };
+
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];     
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({habits : habits}); //왼쪽 key 오른쪽 value habit
+  };
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({habits : habits}); //왼쪽 key 오른쪽 value habit
+  };
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter(item => item.id !== habit.id);
+    this.setState({habist : habits});
+  };
+
+
+  render(){
+    return (
+    <>
+    <Navbar totlaCount = {this.state.habits.filter(item => item.count > 0 ).length}/>
+    <Habits 
+      habits={this.state.habits}
+      onIncrement = {this.handleIncrement}
+      onDecrement = {this.handleDecrement}
+      onDelete = {this.handleDelete}
+    />;
+    </>
+    )
+  }  
 }
 
 export default App;
--- habit-tracker/src/compoents/habits.jsx
+++ habit-tracker/src/compoents/habits.jsx
@@ -3,30 +3,15 @@ import Habit from './habit';
 import HabitAddForm from './habitAddForm';
 
 class Habits extends Component {
-    state = {
-        habits: [
-            { id: 1, name : 'Reading' , count :0},
-            { id: 2, name : 'Running' , count :0},
-            { id: 3, name : 'Coding' , count :0},
-        ],
-    };
-
+ 
     handleIncrement = (habit) => {
-      const habits = [...this.state.habits];     
-      const index = habits.indexOf(habit);
-      habits[index].count++;
-      this.setState({habits : habits}); //왼쪽 key 오른쪽 value habit
+        this.props.onIncrement(habit);
     };
     handleDecrement = (habit) => {
-      const habits = [...this.state.habits];
-      const index = habits.indexOf(habit);
-      const count = habits[index].count - 1;
-      habits[index].count = count < 0 ? 0 : count;
-      this.setState({habits : habits}); //왼쪽 key 오른쪽 value habit
+        this.props.onDecrement(habit);
     };
     handleDelete = (habit) => {
-      const habits = this.state.habits.filter(item => item.id !== habit.id);
-      this.setState({habist : habits});
+        this.props.onDelete(habit);
     };
 
 
