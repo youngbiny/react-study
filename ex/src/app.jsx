2d1b631b220bf25d7a6bb31cb161ead928cd4ce4
youngbiny <ultrabab@naver.com>
2021-07-11 23:48:11 +0900

    a

--- ex/src/app.jsx
+++ ex/src/app.jsx
@@ -1,10 +1,48 @@
 import './app.css';
+import React, { Component } from 'react';
 import Habits from './compoents/habits';
 
-function App() {
-  return (
-    <Habits />
-  );
+class app extends Component {
+ state = {
+    habits : [
+        { id : 1, name : 'Reading', count:0 },
+        { id : 2, name : 'Coding', count:0 },
+        { id : 3, name : 'Running', count:0 },
+    ],
+};
+handleIncrement = (habit) => {
+  const habits = [...this.state.habits];
+  const index = this.state.indexOf(habits);
+  this.habits[index].setState({habits : habits});
+};
+
+handleDecrement = (habit) => {
+  const habits = [...this.state.habits];
+  const index =  this.state.indexOf(habits);
+  const count = habits.count < 0 ? 0 : habits.count - 1;
+  this.habits[index].setState({habits});
+};
+
+handleDelete = (habit) => {
+  const habits = this.state.habits.filter(item => item.id =! habit.id);
+  this.habits.setState({habits});
+};
+
+  render() {
+    return (
+      <>
+        <Habits
+        key = {habit.id}
+        habit = {habit}
+        onIncrement = {this.handleIncrement}
+        onDecrement = {this.handleDecrement}
+        onDelete = {this.handleDelete}
+         />
+      </>
+    );
+  }
 }
+
+export default app;
 
 export default App;
